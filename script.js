7b982cee6eb97e769db837fce40197cea15bc8e6
a <a>
2025-04-01 09:58:42 +0200

    update1

---
+++ script.js
@@ -0,0 +1,34 @@
+g1 = false
+g2 = false
+g3 = false
+g4 = false
+g5 = false
+
+
+nav = document.getElementById('nav');
+box1 = document.getElementById('box1');
+box2 = document.getElementById('box2');
+box3 = document.getElementById('box3');
+box4 = document.getElementById('box4');
+box5 = document.getElementById('box5');
+
+function Go_nav(){
+    nav.scrollIntoView();
+}
+function Go_box1(){
+    box1.scrollIntoView();
+}
+function Go_box2(){
+    box2.scrollIntoView();
+}
+function Go_box3(){
+    box3.scrollIntoView();
+}
+function Go_box4(){
+    box4.scrollIntoView();
+}
+function Go_box5(){
+    box5.scrollIntoView();
+}
+
+
